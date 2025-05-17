171aaf5c3fc0c7bc3975c4e482b4eeb8d426a269
FabboCassa <casarini.fabio@gmail.com>
2025-05-17 14:23:54 +0200

    Implemented modify of insertion

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiAppDataSaving.Models
 {
-    public class Product
+    public class Product : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public double Price { get; set; }
+        private string _name;
+        private double _price;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    OnPropertyChanged(nameof(Price));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -16,12 +16,43 @@ namespace MauiAppDataSaving.ViewModels
         private readonly IProductService _productService;
 
         public ObservableCollection<Product> Products { get; set; } = new();
+
+        private Product _selectedProduct;
+        public Product SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                if (_selectedProduct != value)
+                {
+                    _selectedProduct = value;
+                    OnPropertyChanged(nameof(SelectedProduct));
+                    EditedName = _selectedProduct?.Name ?? string.Empty; // Fix for CS8601  
+                }
+            }
+        }
+
+        private string _editedName;
+        public string EditedName
+        {
+            get => _editedName;
+            set
+            {
+                if (_editedName != value)
+                {
+                    _editedName = value;
+                    OnPropertyChanged(nameof(EditedName));
+                }
+            }
+        }
         public ICommand AddProductCommand { get; }
+        public ICommand SaveProductCommand { get; }
 
         public MainViewModel(IProductService productService)
         {
             _productService = productService;
             AddProductCommand = new Command(async () => await AddProductAsync());
+            SaveProductCommand = new Command(SaveProduct);
             LoadProducts();
         }
 
@@ -41,6 +72,19 @@ namespace MauiAppDataSaving.ViewModels
             Products.Add(newProduct);
         }
 
+        private void SaveProduct()
+        {
+            if (SelectedProduct != null && !string.IsNullOrWhiteSpace(EditedName))
+            {
+                SelectedProduct.Name = EditedName;
+                OnPropertyChanged(nameof(SelectedProduct));
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
